c2089843310599dfabfb7902d2b1adfb17eb3739
pattisoa <pattisoa@OA-S139-17>
2026-02-04 14:13:41 -0500

    items only display if they are inside the inventory list

--- LocalBiohazards-TeamDesign/Assets/Scripts/InteractableObjects/ItemIconDisplay.cs
+++ LocalBiohazards-TeamDesign/Assets/Scripts/InteractableObjects/ItemIconDisplay.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemIconDisplay : MonoBehaviour
 {
+    EventCore eventcore; 
+    Image BackgroundImage;
+    public Inventory inventory;
+    public GameObject ItemIcon;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        BackgroundImage = GetComponent<Image>();
+        ItemIcon = transform.GetChild(0).gameObject;
+        eventcore = GameObject.Find("EventCore").GetComponent<EventCore>();
+        inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
+        BackgroundImage.enabled = false;
+        ItemIcon.SetActive(false);
+        eventcore.addToInventoryEV.AddListener(SetItemActive);
     }
-
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Sets the item display that this has children active and <div>
+    /// Set the image component to from fasle</div>
+    /// PickedUpItem does not get used
+    /// </summary>
+    /// <param name="PickedUpItem"></param>
+    void SetItemActive(Items PickedUpItem)
     {
-        
+        foreach (Items inventory in inventory.itemList)
+        {
+            if(inventory == ItemIcon.GetComponent<ItemImageInteraction>().Item)
+            {
+                BackgroundImage.enabled = true;
+                ItemIcon.SetActive(true);
+            }
+        }
     }
+  
 }
