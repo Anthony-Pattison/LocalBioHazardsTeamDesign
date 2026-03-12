ed4d8f22235c12eea0c3324f5f11eba4bb6920a3
valor87 <134859690+valor87@users.noreply.github.com>
2026-03-12 16:33:13 -0400

    the npcs move around and have a wonder script

--- LocalBiohazards-TeamDesign/Assets/Scripts/NodeCanvusScripts/ActionTasks/changeRoomAT.cs
+++ LocalBiohazards-TeamDesign/Assets/Scripts/NodeCanvusScripts/ActionTasks/changeRoomAT.cs
@@ -1,12 +1,15 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
-
+using UnityEngine.AI;
+using UnityEngine;
+using ParadoxNotion.Serialization.FullSerializer;
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class changeRoomAT : ActionTask {
 
 		public locationClass locations;
+		public BBParameter<NavMeshAgent> navAgentBBP;
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
@@ -17,12 +20,16 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+			navAgentBBP.value.SetDestination(locations.aiLocations[0].locationTransform.position);
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
+            if (Vector3.Distance(agent.transform.position, locations.aiLocations[0].locationTransform.position) < 1)
+			{
+				Debug.Log("made it to the position");
+				EndAction();
+			}
 		}
 
 		//Called when the task is disabled.
---
+++ LocalBiohazards-TeamDesign/Assets/Scripts/NodeCanvusScripts/ActionTasks/wonderAroundRoomAT.cs
@@ -0,0 +1,68 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Serialization.FullSerializer;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem.Composites;
+using static UnityEditor.FilePathAttribute;
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class wonderAroundRoomAT : ActionTask {
+		public BBParameter<NavMeshAgent> navAgent;
+		public float randomDistance;
+		Vector3 wonderSpot;
+		Vector3 Destination;
+		public Transform wonderSpotTransform;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+			wonderSpot = wonderSpotTransform.position;
+			setNewDestination();
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			float distanceToDestination = Vector3.Distance(agent.transform.position, Destination);
+			Debug.DrawLine(agent.transform.position, Destination, Color.red);
+            Debug.Log(distanceToDestination);
+			if ( distanceToDestination < 2.0f)
+			{
+				setNewDestination();
+            }
+		}
+
+		void setNewDestination()
+		{
+			Vector3 Temp = randomDistance * Random.insideUnitSphere + wonderSpotTransform.position;
+			NavMeshHit hit = new NavMeshHit();
+			if (!NavMesh.SamplePosition(Temp, out hit, randomDistance, NavMesh.AllAreas))
+			{
+				Debug.LogError("didnt find a location");
+				setNewDestination();
+			}
+			else
+			{
+                Destination = hit.position;
+				navAgent.value.SetDestination(Destination);
+			}
+		}
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
