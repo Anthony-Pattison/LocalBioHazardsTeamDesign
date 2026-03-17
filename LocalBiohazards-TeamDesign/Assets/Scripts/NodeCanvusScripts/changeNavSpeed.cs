740f10dcf943137509fac8e1f6d5425c67c68665
valor87 <134859690+valor87@users.noreply.github.com>
2026-03-17 01:47:31 -0400

    fixing some minor bugs with the ui and the npcs

--- LocalBiohazards-TeamDesign/Assets/Scripts/NodeCanvusScripts/changeNavSpeed.cs
+++ LocalBiohazards-TeamDesign/Assets/Scripts/NodeCanvusScripts/changeNavSpeed.cs
@@ -4,7 +4,21 @@ using UnityEngine.AI;
 public class changeNavSpeed : MonoBehaviour
 {
     public NavMeshAgent agent;
-    
+    Transform cameraTransform;
+    private void Start()
+    {
+        cameraTransform = GameObject.Find("CameraHolder").transform;
+    }
+
+    private void Update()
+    {
+        Vector3 CamPos = cameraTransform.position;
+        if (cameraTransform != null)
+        {
+            CamPos.y = transform.position.y;
+            transform.LookAt(CamPos);
+        }
+    }
     public void changeSpeed(float newSpeed)
     {
         agent.speed = newSpeed;
